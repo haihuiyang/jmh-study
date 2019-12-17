415d99658747a5a9734a3e1a046b6c88fd7a1c60
HappyFeet <yanghaihui@betalpha.com>
2019-12-17 22:09:27 +0800

    update StringBuilderVsStringBufferBenchmark

--- src/main/java/org/openjdk/jmh/custom/StringBuilderVsStringBufferBenchmark.java
+++ src/main/java/org/openjdk/jmh/custom/StringBuilderVsStringBufferBenchmark.java
@@ -17,8 +17,8 @@ import static java.util.stream.Collectors.toList;
  * @since Dec 13, 2019
  */
 
-@BenchmarkMode({Mode.Throughput})
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Fork(value = 1, jvmArgs = {"-Xms1G", "-Xmx1G"})
 @Warmup(iterations = 3)
 @State(Scope.Benchmark)
@@ -37,11 +37,21 @@ public class StringBuilderVsStringBufferBenchmark {
     }
 
     @Benchmark
+    public String plusInLoop(MyState state) {
+        String str = "";
+        for (int i = 0; i < iterations; i++) {
+            str = str + state.preparedStrList.get(i);
+        }
+        return str;
+    }
+
+    @Benchmark
     public String stringBuilder(MyState state) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < iterations; i++) {
             sb.append(state.preparedStrList.get(i));
         }
+        String str = new String("hello") + " world";
         return sb.toString();
     }
 
@@ -54,10 +64,84 @@ public class StringBuilderVsStringBufferBenchmark {
         return sb.toString();
     }
 
+    @Benchmark
+    public String stringBuilderWithCapacity(MyState state) {
+        StringBuilder sb = new StringBuilder(iterations * 5);
+        for (int i = 0; i < iterations; i++) {
+            sb.append(state.preparedStrList.get(i));
+        }
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String stringBuilderWithoutCapacity(MyState state) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < iterations; i++) {
+            sb.append(state.preparedStrList.get(i));
+        }
+        return sb.toString();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.EXCLUDE)
+    public String byPlusAppendWithoutCompile(MyState state) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(state.mike + state.daniel + state.john + state.martin);
+        return sb.toString();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.EXCLUDE)
+    public String byChainAppendWithoutCompile(MyState state) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(state.mike).append(state.daniel).append(state.john).append(state.martin);
+        return sb.toString();
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.EXCLUDE)
+    public String byEachRowAppendWithoutCompile(MyState state) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(state.mike);
+        sb.append(state.daniel);
+        sb.append(state.john);
+        sb.append(state.martin);
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String byPlusAppendWithCompile(MyState state) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(state.mike + state.daniel + state.john + state.martin);
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String byChainAppendWithCompile(MyState state) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(state.mike).append(state.daniel).append(state.john).append(state.martin);
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String byEachRowAppendWithCompile(MyState state) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(state.mike);
+        sb.append(state.daniel);
+        sb.append(state.john);
+        sb.append(state.martin);
+        return sb.toString();
+    }
+
     @State(Scope.Benchmark)
     public static class MyState {
+        String mike = "mike";
+        String daniel = "daniel";
+        String john = "john";
+        String martin = "martin";
+
         List<String> preparedStrList = IntStream
-                .rangeClosed(1, 10000)
+                .rangeClosed(10001, 20000)
                 .boxed()
                 .map(Object::toString)
                 .collect(toList());
